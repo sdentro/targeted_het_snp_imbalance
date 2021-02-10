87f99b2c7766099d104ba2323787a84e7b108aec
Stefan Dentro <sd11@sanger.ac.uk>
2021-02-10 15:22:38 +0000

    Adding more empty dirs

--- reference/split.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-zcat mgp.v5.merged.snps_all.dbSNP142.vcf.gz | grep -v ^\# | cut -f 1,2,4,5 | grep -v "," | awk '{print > ("mm10_SNP_alleles_" $1 ".txt")}'
-mv mm10_SNP_alleles_X.txt mm10_SNP_alleles_20.txt
-mv mm10_SNP_alleles_Y.txt mm10_SNP_alleles_21.txt
-rm mm10_SNP_alleles_MT.txt
