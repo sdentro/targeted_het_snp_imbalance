14ded0d8058de2dff592e3b2e261407fb79fb074
Stefan Dentro <sd11@sanger.ac.uk>
2021-09-10 12:05:46 +0100

    Add correct chrX allele counting and adjust to latest Battenberg code changes

--- pipeline/allelecount/submit.sh
+++ pipeline/allelecount/submit.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-alleleprefix="../../reference/mm10_SNP_alleles_"
+alleleprefix=`cat ../config.txt | grep human_g1000lociprefix | cut -f 2`
 mem=15000
 for item in `find ../../raw_data | grep "\.bam" | grep -v bai`; do
 	sam=`basename ${item} | cut -f 1 -d .`
-	for i in $(seq 1 21); do
+	for i in $(seq 1 22); do
 		bsub -R"select[mem>${mem}] rusage[mem=${mem}]" -M${mem} -o logs/${sam}_${i}_%J.out "alleleCounter -d -l ${alleleprefix}${i}.txt -b ${item} -o output/${sam}_alleleFrequencies_chr${i}.txt"
 	done
+	i=23
+	bsub -R"select[mem>${mem}] rusage[mem=${mem}]" -M${mem} -o logs/${sam}_${i}_%J.out "alleleCounter -d -l ${alleleprefix}${i}.txt -b ${item} -o output/${sam}_alleleFrequencies_chrX.txt"
 done
--- pipeline/allelecount/submit_todo.sh
+++ pipeline/allelecount/submit_todo.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-alleleprefix="../../reference/mm10_SNP_alleles_"
+alleleprefix=`cat ../config.txt | grep human_g1000lociprefix | cut -f 2`
 mem=15000
 nlines=`cat todo.txt | wc -l`
 #for item in `find ../../raw_data | grep "\.bam" | grep -v bai | tail -n 684`; do
 for j in $(seq 1 ${nlines}); do
 	sam=`cut -f 1 todo.txt | head -n ${j} | tail -n 1`
 	i=`cut -f 2 todo.txt | head -n ${j} | tail -n 1`
+	if [[ "${i}" == 23 ]]; then
+		chrom="X"
+	else
+		chrom=${i}
+	fi
 	item=`find ../../raw_data | grep "\.bam" | grep -v bai | grep "${sam}\."`
-	bsub -R"select[mem>${mem}] rusage[mem=${mem}]" -M${mem} -o logs/${sam}_${i}_%J.out "alleleCounter -l ${alleleprefix}${i}.txt -b ${item} -o output/${sam}_alleleFrequencies_chr${i}.txt"
+	bsub -R"select[mem>${mem}] rusage[mem=${mem}]" -M${mem} -o logs/${sam}_${i}_%J.out "alleleCounter -l ${alleleprefix}${i}.txt -b ${item} -o output/${sam}_alleleFrequencies_chr${chrom}.txt"
 done
